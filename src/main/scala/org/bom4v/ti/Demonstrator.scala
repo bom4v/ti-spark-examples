23e2e73a4439633efaa3b0268867c8865bcc7221
Radoslaw Kita <rkita@op.pl>
2017-12-11 15:47:54 +0100

    new lines in the example file

--- src/main/scala/org/bom4v/ti/Demonstrator.scala
+++ src/main/scala/org/bom4v/ti/Demonstrator.scala
@@ -19,6 +19,11 @@ import org.bom4v.ti.models.customers.CustomerAccount.AccountModelForChurn
 import org.bom4v.ti.models.calls.CallsModel.CallEvent
 import org.bom4v.ti.serializers.customers.CustomerAccount._
 import org.bom4v.ti.serializers.calls.CallsModel._
+//data preparation
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.DateType
+
 
 object Demonstrator extends App {
 
@@ -192,6 +197,107 @@ object Demonstrator extends App {
     """CASE double(round(prediction)) = label WHEN true then 1 ELSE 0 END as equal"""
   )
   equalp.show()
+
+//data preparation
+val spark = SparkSession.builder().getOrCreate()
+
+val cdr_data_t = spark.read.option("header","true").option("inferSchema","true").option("delimiter", "^").csv("cdr_example.csv")
+
+val cdr_data_temp = cdr_data_t.selectExpr("specificationVersionNumber",
+ 					"releaseVersionNumber",
+ 					 "fileName",
+ 					 "fileAvailableTimeStamp",
+ 					 "fileUtcTimeOffset",
+ 					 "sender",
+ 					 "recipient",
+ 					 "sequenceNumber",
+ 					 "cast(callEventsCount as Int) callEventsCount",
+ 					 "eventType",
+ 					 "cast(imsi as String) imsi",
+ 					 "cast(imei as String) imei",
+ 					 "callEventStartTimeStamp",
+ 					 "utcTimeOffset",
+ 					 "callEventDuration",
+ 					 "causeForTermination",
+ 					 "accessPointNameNI",
+ 					 "accessPointNameOI",
+ 					 "cast(dataVolumeIncoming as Int) dataVolumeIncoming",
+ 					 "cast(dataVolumeOutgoing as Int) dataVolumeOutgoing",
+ 					 "sgsnAddress",
+ 					 "ggsnAddress",
+ 					 "chargingId",
+ 					 "cast(chargeAmount as Int) chargeAmount",
+ 					 "teleServiceCode",
+ 					 "bearerServiceCode",
+ 					 "supplementaryServiceCode",
+ 					 "cast(dialledDigits as String) dialledDigits",
+ 					 "cast(connectedNumber as String) connectedNumber",
+ 					 "cast(thirdPartyNumber as String) thirdPartyNumber",
+ 					 "cast(callingNumber as String) callingNumber",
+ 					 "recEntityId",
+ 					 "callReference",
+ 					 "locationArea",
+ 					 "cellId",
+ 					 "cast(msisdn as String) msisdn",
+ 					 "servingNetwork")
+
+
+val cdr_data = cdr_data_temp.withColumn("callEventStartDay",cdr_data_temp("callEventStartTimeStamp").cast(DateType))
+
+val callEventDuration=cdr_data.groupBy("imsi").agg(mean("callEventDuration"), min("callEventDuration"), 
+			max("callEventDuration"), sum("callEventDuration"))
+
+val prefix = "callEventDuration"
+val renamedColumns = callEventDuration.columns.map(c=> callEventDuration(c).as(s"$prefix$c"))
+val callEventDurationRename = callEventDuration.select(renamedColumns: _*)
+
+
+
+val dataVolumeIncoming=cdr_data.groupBy("imsi").agg(mean("dataVolumeIncoming"), min("dataVolumeIncoming"), 
+			max("dataVolumeIncoming"), sum("dataVolumeIncoming"))
+
+val prefix = "dataVolumeIncoming"
+val renamedColumns = dataVolumeIncoming.columns.map(c=> dataVolumeIncoming(c).as(s"$prefix$c"))
+val dataVolumeIncomingRename = dataVolumeIncoming.select(renamedColumns: _*)
+
+
+
+val dataVolumeOutgoing=cdr_data.groupBy("imsi").agg(mean("dataVolumeOutgoing"), min("dataVolumeOutgoing"), 
+			max("dataVolumeOutgoing"), sum("dataVolumeOutgoing"))
+
+val prefix = "dataVolumeOutgoing"
+val renamedColumns = dataVolumeOutgoing.columns.map(c=> dataVolumeOutgoing(c).as(s"$prefix$c"))
+val dataVolumeOutgoingRename = dataVolumeOutgoing.select(renamedColumns: _*)
+
+
+
+val chargeAmount=cdr_data.groupBy("imsi").agg(mean("chargeAmount"), min("chargeAmount"), 
+			max("chargeAmount"), sum("chargeAmount"))
+
+val prefix = "chargeAmount"
+val renamedColumns = chargeAmount.columns.map(c=> chargeAmount(c).as(s"$prefix$c"))
+val chargeAmountRename = chargeAmount.select(renamedColumns: _*)
+
+
+val callingNumberN = cdr_data.groupBy("imsi").agg(countDistinct("callingNumber"))
+val locationAreaN = cdr_data.groupBy("imsi").agg(countDistinct("locationArea"))
+val cellIdN = cdr_data.groupBy("imsi").agg(countDistinct("cellId"))
+val activeDaysN = cdr_data.groupBy("imsi").agg(countDistinct("callEventStartDay"))
+
+
+val callingNumbersTechN = cdr_data.groupBy("imsi","callingNumber").count()
+val locationAreasTechN = cdr_data.groupBy("imsi","locationArea").count()
+val cellIdsTechN = cdr_data.groupBy("imsi","cellId").count()
+val activeDaysTechN = cdr_data.groupBy("imsi","callEventStartDay").count()
+
+val locationAreasTech = cdr_data.groupBy("imsi","locationArea").pivot("locationArea").agg(mean("callEventDuration"), min("callEventDuration"), 
+			max("callEventDuration"), sum("callEventDuration"))
+val cellIdsTech = cdr_data.groupBy("imsi","cellId").pivot("cellId").agg(mean("callEventDuration"), min("callEventDuration"), 
+			max("callEventDuration"), sum("callEventDuration"))
+val activeDaysTech = cdr_data.groupBy("imsi","callEventStartDay").pivot("callEventStartDay").agg(mean("callEventDuration"), min("callEventDuration"), 
+			max("callEventDuration"), sum("callEventDuration"))
+
+
 
   // Stop Spark
   spark.stop()
